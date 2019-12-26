b4f495dfc2faa170b79534bd6d81539416a7a76b
Anna Wieteska <awieteska39@gmail.com>
2019-12-26 19:29:22 +0100

    kfj

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def main():
     create_layout()
     
 def load_homepage() -> None:
-    st.image("/home/anna/niemoje/streamlit_guide/iwakka.png",
+    st.image("https://github.com/209614-student/streamlit_iWakka/blob/master/iwakka.png",
              use_column_width=True)
     st.markdown("> It' s a dashboard do analize data!")
     st.write("Hello! This dashboard will help you to analize data from our device iWakka "
